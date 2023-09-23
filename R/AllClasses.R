1ee1ed14c1ebccbe5d8675d795ffb1b0a6a46cbe
marouenbg <marouen.b.guebila@gmail.com>
2023-09-23 14:02:03 -0400

    update export class

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,2 +1,6 @@
+# Class definition
+#' @exportClass pandaObj
+#' @export pandaObj
+#' 
 pandaObj <- setClass("panda", slots=c("regNet","coregNet","coopNet","numGenes","numTFs","numEdges"))
 setMethod("show","panda",function(object){print.panda(object)})
